b6bc43bbb2a635177a522f0bade441fae2fe299b
huihyeon06 <agnea0829@gmail.com>
2023-09-12 16:11:33 +0900

    [feat] : 버튼 누르면 창 변경

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,22 @@
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 public class Main extends JFrame {
-    public Main(){
+    private final CardLayout cardLayout = new CardLayout();
+    private final JPanel cardPanel = new JPanel(cardLayout);
+
+    public Main() {
         setTitle("Savearth");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         Container c = getContentPane();
-        ImageIcon login = loadImageIcon("images/id.png");
+
+        ImageIcon login = loadImageIcon("Savearth/images/id.png");
 
         if (login != null) {
             int width = login.getIconWidth();
@@ -18,18 +24,43 @@ public class Main extends JFrame {
             JButton imageButton = new JButton(login);
             imageButton.setPreferredSize(new Dimension(width, height));
 
-            c.add(imageButton);
+            // 첫 번째 카드
+            JPanel card1 = new JPanel(new GridBagLayout());
+            GridBagConstraints gbc = new GridBagConstraints();
+            gbc.gridx=0;
+            gbc.gridy=1;
+            gbc.anchor=GridBagConstraints.SOUTH;
+            card1.add(imageButton, gbc);
+
+            // 두 번째 카드
+            JPanel card2 = new JPanel();
+            card2.add(new JLabel("This is the second card."));
+
+            // 카드에 이름 지정
+            cardPanel.add(card1, "Card 1");
+            cardPanel.add(card2, "Card 2");
+
+            // 버튼을 클릭하여 다음 카드로 전환
+            imageButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    cardLayout.next(cardPanel); // 다음 카드로 이동
+                }
+            });
+
+            c.add(cardPanel);
         }
 
         setSize(1100, 600);
         setVisible(true);
     }
+
     public static void main(String[] args) {
-        Main m = new Main();
+        new Main();
     }
+
     private static ImageIcon loadImageIcon(String path) {
         try {
-            // 이미지 파일을 로드하여 ImageIcon으로 반환
             BufferedImage image = ImageIO.read(new File(path));
             return new ImageIcon(image);
         } catch (IOException e) {
